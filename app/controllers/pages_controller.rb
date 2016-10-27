84e25c16eb736241eb9b94b64208264b2e60cff7
João Rosa <jmrfrosa92@gmail.com>
2016-10-27 10:03:16 +0100

    Added dynamic size choice for canvas (bad implementation)

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,6 +1,23 @@
 class PagesController < ApplicationController
 
   def show
+    @width = 800
+    @height = 600
+  end
+
+  def update
+    @size = params[:size]
+    case @size.fetch(:size)
+    when "640x480"
+      @width = 640
+      @height = 480
+    when "800x600"
+      @width = 800
+      @height = 600
+    when "1024x768"
+      @width = 1024
+      @height = 768
+    end
   end
 
 end
