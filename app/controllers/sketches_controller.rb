b4e4935748e6fd4e77e2942eac09de7c75b0e4c0
João Rosa <jmrfrosa92@gmail.com>
2016-10-28 12:30:10 +0100

    Fleshed out methods in sketches controller

--- app/controllers/sketches_controller.rb
+++ app/controllers/sketches_controller.rb
@@ -1,4 +1,5 @@
 class SketchesController < ApplicationController
+  before_action :set_post, only: [:show, :create, :update, :destroy]
 
   def index
     @sketches = Sketch.all
@@ -7,13 +8,34 @@ class SketchesController < ApplicationController
   def show
   end
 
+  def edit
+  end
+
   def create
+    @sketch = Sketch.new(sketch_params)
   end
 
   def update
+    if @sketch.update(sketch_params)
+      redirect_to @sketch
+    else
+      render :edit
+    end
   end
 
   def destroy
+    @post.destroy
+    redirect_to sketches_url
   end
+
+  private
+
+    def set_post
+      @sketch = Sketch.find(params[:id])
+    end
+
+    def sketch_params
+      params.require(:sketch).permit(:title, :description, :code)
+    end
 
 end
