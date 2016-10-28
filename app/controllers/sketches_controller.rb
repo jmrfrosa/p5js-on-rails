384cebfa0566b7e74e5c8469c80673c9d3ecb77c
João Rosa <jmrfrosa92@gmail.com>
2016-10-28 11:59:42 +0100

    Created sketch controller with default CRUD actions (still empty)

---
+++ app/controllers/sketches_controller.rb
@@ -0,0 +1,19 @@
+class SketchesController < ApplicationController
+
+  def index
+    @sketches = Sketch.all
+  end
+
+  def show
+  end
+
+  def create
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+
+end
